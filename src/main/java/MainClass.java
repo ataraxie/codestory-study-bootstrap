9367aa2946bf4d56a09f3ee45d1eebad2a2c154c
Felix Grund <felix.grund@scandio.de>
2017-03-09 14:12:00 -0800

    implemented bootstrap2

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,7 +1,16 @@
+import java.util.List;
+
 public class MainClass {
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("HALLO");
+		String json = "[{name: 'Nick'}, {name: 'Felix'}]";
+		List<Person> entities = Util.getEntities(json);
+		System.out.println("Result entities: " + entities.toString());
+
+		String version1 = "3.1.1";
+		String version2 = "3.2";
+		int versionCompare = Util.compareVersions(version1, version2);
+		System.out.println("Result version compare: " + versionCompare);
 	}
 
 }
--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,5 +1,46 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Util {
 
+	public static List<Person> getEntities(String json) throws IllegalStateException {
+		// The list we want to return
+		List<Person> entities = new ArrayList<Person>();
+		Gson gson = new Gson();
+		JsonElement object = new JsonParser().parse(json);
+		JsonArray array = object.getAsJsonArray();
+		// JsonArray implements Iterable and can be used a lot like a List
+		for (JsonElement element : array) {
+			JsonElement entityObject = element.getAsJsonObject();
+			// for each object in the json, create our custom object
+			if (entityObject.isJsonObject()) {
+				entities.add(gson.fromJson(entityObject, Person.class));
+			}
+		}
+		return entities;
+	}
 
+	public static int compareVersions(String str1, String str2) {
+		String[] vals1 = str1.split("\\.");
+		String[] vals2 = str2.split("\\.");
+		int i = 0;
+		// set index to first non-equal ordinal or length of shortest version string
+		while (i < vals1.length && i < vals2.length && vals1[i].equals(vals2[i])) {
+			i++;
+		}
+		// compare first non-equal ordinal number
+		if (i < vals1.length && i < vals2.length) {
+			int diff = Integer.valueOf(vals1[i]).compareTo(Integer.valueOf(vals2[i]));
+			return Integer.signum(diff);
+		}
+		// the strings are equal or one string is a substring of the other
+		// e.g. "1.2.3" = "1.2.3" or "1.2.3" < "1.2.3.4"
+		return Integer.signum(vals1.length - vals2.length);
+	}
 
 }
